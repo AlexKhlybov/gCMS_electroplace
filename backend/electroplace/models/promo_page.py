599bf497349eaf47bac460b8d0d4e98a92a55b41
AlexKhlybov <aghlybov@gmail.com>
2021-08-05 23:56:30 +0300

    Added menu

---
+++ backend/electroplace/models/promo_page.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from garpix_page.models import BasePage
+from .contact_page import ContactPage
+
+
+class ProductsCategoryPage(BasePage):
+    name = models.CharField(verbose_name="Категория", max_length=32, unique=True)
+    desc_category = models.TextField(verbose_name="Описание", blank=True)
+
+    template = 'pages/category_page.html'
+
+    def get_context(self, request=None, *args, **kwargs):
+        context = super().get_context(request=request, *args, **kwargs)
+        context['address'] = ContactPage.objects.all().first()
+        return context
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+        
