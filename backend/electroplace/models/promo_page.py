3a0a239cadca4a92386ba89a6b6f16207a8e1acb
AlexKhlybov <aghlybov@gmail.com>
2021-08-07 21:54:14 +0300

    UPD base.html

--- backend/electroplace/models/promo_page.py
+++ backend/electroplace/models/promo_page.py
@@ -2,17 +2,16 @@ from django.db import models
 
 from garpix_page.models import BasePage
 from .contact_page import ContactPage
+from .product_page import ProductPage
 
 
-class ProductsCategoryPage(BasePage):
-    name = models.CharField(verbose_name="Категория", max_length=32, unique=True)
-    desc_category = models.TextField(verbose_name="Описание", blank=True)
-
+class PromoPage(BasePage):
     template = 'pages/category_page.html'
 
     def get_context(self, request=None, *args, **kwargs):
         context = super().get_context(request=request, *args, **kwargs)
         context['contacts'] = ContactPage.objects.all().first()
+        context['products'] = ProductPage.on_site.filter(is_active=True, parent=kwargs['object'])
         return context
 
     class Meta:
